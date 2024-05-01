3c1ad17552cdcbd4455c69d24dd8fe79ffcda11d
wagnerSfarias <wagner@universalhost.com.br>
2024-04-30 23:25:48 -0300

    refactor: inserting styles react-select

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -9,5 +9,14 @@ export default createGlobalStyle`
     box-sizing: border-box;
     font-family: 'Poppins', sans-serif;
     outline: none;
+
+   body{
+ 
+    .react-select-status__option{
+        text-overflow: ellipsis;
+        overflow: hidden;
+        font-size: 14px;
+    }
+ }
 }
 `
