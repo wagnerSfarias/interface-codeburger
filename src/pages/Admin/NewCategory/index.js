8bd7541e3479406f08fd1053c1c6da6622845781
wagnerSfarias <wagner@universalhost.com.br>
2024-05-15 22:45:56 -0300

    feat: creating page add category

---
+++ src/pages/Admin/NewCategory/index.js
@@ -0,0 +1,173 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import UploadImage from '@mui/icons-material/CloudUpload'
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import React, { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useHistory } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import * as Yup from 'yup'
+
+import { ErrorMessage } from '../../../components'
+import paths from '../../../constants/paths'
+import { useUser } from '../../../hooks/UserContext'
+import api from '../../../services/api'
+import {
+  Container,
+  Label,
+  Input,
+  Button,
+  LabelUpload,
+  EditIcon
+} from './styles'
+
+export default function NewCategory() {
+  const [fileName, setFileName] = useState(null)
+  const [categories, setCategories] = useState([])
+  const [error, setError] = useState(false)
+  const { push } = useHistory()
+  const { logout } = useUser()
+
+  const schema = Yup.object().shape({
+    name: Yup.string().required('Digite o nome do produto'),
+    file: Yup.mixed()
+      .test('required', 'Carregue uma imagem', value => {
+        return value?.length > 0
+      })
+      .test(
+        'type',
+        'Carregue apenas arquivos de extensões png, jpg, jpeg.',
+        value => {
+          return (
+            value[0]?.type === 'image/jpeg' || value[0]?.type === 'image/png'
+          )
+        }
+      )
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm({ resolver: yupResolver(schema) })
+
+  useEffect(() => {
+    async function loadCategoreis() {
+      try {
+        const response = await api.get('categories')
+        setCategories(response.data)
+      } catch (err) {}
+    }
+    loadCategoreis()
+  }, [])
+
+  const onSubmit = async data => {
+    const categoryDataFormData = new FormData()
+
+    categoryDataFormData.append('name', data.name)
+    categoryDataFormData.append('file', data.file[0])
+
+    try {
+      const response = await api.post('categories', categoryDataFormData, {
+        validateStatus: () => true
+      })
+
+      if (response.status === 200 || response.status === 201) {
+        toast.success('Categoria cadastrado com sucesso')
+        setCategories([...categories, response.data])
+
+        reset({ name: '', file: {} })
+        setFileName(null)
+        setError(false)
+      } else if (response.status === 400) {
+        toast.error('Nome da categoria já existe, tente usar outro nome.')
+        setError(true)
+      } else if (response.status === 401) {
+        logout()
+        toast.error('Ocorreu um erro com sua autenticação! Tente novamente.')
+
+        setTimeout(() => {
+          push('/login')
+        }, 2000)
+      } else {
+        throw new Error()
+      }
+    } catch (err) {}
+  }
+
+  function editCategory(category) {
+    push(paths.EditCategory, { category })
+  }
+
+  return (
+    <Container>
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <h2>Criar Categoria</h2>
+        <div>
+          <Label>Nome</Label>
+          <Input type="text" {...register('name')} errorExist={error} />
+          <ErrorMessage>{errors.name?.message}</ErrorMessage>
+          {error && <ErrorMessage>Nome da categoria já existe.</ErrorMessage>}
+        </div>
+
+        <div>
+          <LabelUpload>
+            {fileName || (
+              <>
+                <UploadImage />
+                Carregue a imagem
+              </>
+            )}
+
+            <input
+              {...register('file')}
+              type="file"
+              accept="image/png, image/jpeg"
+              onChange={value => setFileName(value.target.files[0]?.name)}
+            />
+          </LabelUpload>
+          <ErrorMessage>{errors.file?.message}</ErrorMessage>
+        </div>
+
+        <Button>Criar</Button>
+      </form>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell className="close"></TableCell>
+              <TableCell className="edit">Editar</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {categories &&
+              categories.map(category => (
+                <TableRow
+                  key={category.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {category.name}
+                  </TableCell>
+                  <TableCell className="cell-image">
+                    <img src={category.url} alt="imagem-categoria" />
+                  </TableCell>
+                  <TableCell className="edit">
+                    <EditIcon onClick={() => editCategory(category)} />
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  )
+}
---
+++ src/pages/Admin/NewCategory/styles.js
@@ -0,0 +1,132 @@
+import Edit from '@mui/icons-material/Edit'
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 5%;
+  gap: 20px;
+
+  .MuiTableContainer-root {
+    width: 80%;
+    margin: 2% auto 10%;
+  }
+
+  .MuiTable-root {
+    table-layout: fixed;
+    min-width: 0;
+  }
+
+  .MuiTableCell-root {
+    width: 80%;
+    min-width: 0px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .cell-image {
+    text-align: center;
+
+    img {
+      width: 70px;
+      height: 65px;
+      border-radius: 5px;
+    }
+  }
+  .edit {
+    width: 50%;
+    padding: 0;
+    text-align: center;
+  }
+
+  form {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+    width: 60%;
+    padding: 30px 30px 80px;
+    box-shadow: 3px 2px 10px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 4px;
+
+    h2 {
+      font-weight: 500;
+    }
+  }
+
+  @media screen and (max-width: 970px) {
+    form {
+      width: 80%;
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    .MuiTableContainer-root {
+      width: 94%;
+    }
+
+    form {
+      padding: 30px 30px 20%;
+      width: 94%;
+    }
+  }
+`
+export const Label = styled.p`
+  font-size: 14px;
+`
+export const Input = styled.input`
+  height: 40px;
+  background: #e5e5e5;
+  border-radius: 8px;
+  font-size: 15px;
+  border: ${props => (props.errorExist ? '1px solid #cc1717' : 'none')};
+  width: 90%;
+  padding-left: 10px;
+  box-shadow: 3px 5px 10px 5px rgba(0, 0, 0, 0.15);
+`
+export const Button = styled.button`
+  padding: 2% 5%;
+  background-color: #d92419;
+  border-radius: 4px;
+  border: none;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 500;
+  position: absolute;
+  bottom: 2%;
+  right: 2%;
+  cursor: pointer;
+
+  @media screen and (max-width: 480px) {
+    bottom: 8%;
+    right: 5%;
+  }
+`
+
+export const LabelUpload = styled.label`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
+  border: 2px dashed #7f7f7f;
+  width: 70%;
+  gap: 10px;
+  padding: 10px;
+  font-size: 14px;
+
+  input {
+    opacity: 0;
+    width: 1px;
+  }
+
+  @media screen and (max-width: 480px) {
+    width: 90%;
+  }
+`
+
+export const EditIcon = styled(Edit)`
+  cursor: pointer;
+  color: #323d5d;
+`
